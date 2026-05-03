9c0e419a261f51d9d39ba9d97c7b3b01b4ba6763
ksalsabilacaca <salsammm31@gmail.com>
2026-05-03 12:15:02 +0700

    update backend apus index.js masukin ke api dan ubah env

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,22 @@
-const app = require("../index"); 
+const express = require("express");
+const cors = require("cors");
+const foodRoutes = require("../backend/routes/foodRoutes");
+const connectDB = require("../backend/config/db");
 
-module.exports = app;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/foods", foodRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API jalan nich");
+});
+
+const serverless = require("serverless-http");
+
+module.exports = async (req, res) => {
+  await connectDB();
+  return serverless(app)(req, res);
+};
--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://salsabilamaharani_db_user:12345678caca@cluster0.t8cjwnv.mongodb.net/food-logger"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error);
